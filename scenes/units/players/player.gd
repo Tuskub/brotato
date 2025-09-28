599e69cdf125b35bf1f99c71f087f050618155fc
Tuskub <alexander.kuznetsov@microimpuls.com>
2025-09-28 10:19:07 +0500

    Add Unit and Player scene with animation

---
+++ scenes/units/players/player.gd
@@ -0,0 +1,81 @@
+extends Unit
+class_name Player
+
+@export var dash_duration := 0.5
+@export var dash_speed_multi := 1.5
+@export var dash_cooldown := 0.5
+
+@onready var dash_timer: Timer = $DashTimer
+@onready var dash_cooldown_timer: Timer = $DashCooldownTimer
+@onready var collission: CollisionShape2D = $CollisionShape2D
+
+var move_dir: Vector2
+var is_dashing := false
+var is_dash_available := true 
+
+
+func _ready() -> void:
+	dash_timer.one_shot = true
+	dash_timer.wait_time = dash_duration
+	
+	dash_cooldown_timer.one_shot = true
+	dash_cooldown_timer.wait_time = dash_cooldown
+
+
+func _process(delta: float) -> void:
+	move_dir = Input.get_vector(
+		"move_left",
+		"move_right",
+		"move_up",
+		"move_down"
+	)
+	
+	var current_velocity := move_dir * 500
+	
+	if is_dashing:
+		current_velocity *= dash_speed_multi
+	
+	position += current_velocity * delta
+	
+	if can_dash():
+		start_dash()
+	
+	update_animations()
+	update_rotation()
+
+
+func update_animations() -> void:
+	if move_dir.length() > 0:
+		animation.play('move')
+	else:
+		animation.play('idle')
+
+
+func update_rotation() -> void:
+	if move_dir == Vector2.ZERO || move_dir.x == 0.0:
+		return
+	
+	var direction = -1 if move_dir.x >= 0.1 else 1
+	visuals.scale = Vector2(direction * 0.5, 0.5)
+	
+
+func start_dash() -> void:
+	is_dashing = true
+	dash_timer.start()
+	visuals.modulate.a = 0.5
+	collission.set_deferred("disabled", true)
+	
+func can_dash() -> bool:
+	return not is_dashing and\
+	dash_cooldown_timer.is_stopped() and\
+	Input.is_action_just_pressed('dash') and\
+	move_dir != Vector2.ZERO
+
+
+
+func _on_dash_timer_timeout() -> void:
+	is_dashing = false
+	visuals.modulate.a = 1.0
+	move_dir = Vector2.ZERO
+	collission.set_deferred("disabled", false)
+	dash_cooldown_timer.start()
---
+++ scenes/units/unit.gd
@@ -0,0 +1,6 @@
+extends Node2D
+class_name Unit
+
+@onready var visuals: Node2D = %Visuals
+@onready var sprite: Node2D = %Sprite
+@onready var animation: AnimationPlayer = $AnimationPlayer
