c0d69738c2497b7a22930d5a26af74481b2d8ac5
Tuskub <domenik.4@mail.ru>
2025-09-30 23:29:42 +0500

    feat: implement floating combat text for damage and blocking

--- autoloads/global.gd
+++ autoloads/global.gd
@@ -1,7 +1,11 @@
 extends Node
 
+signal on_create_block_text(unit: Node2D)
+signal on_create_damage_text(unit: Node2D, hitbox: HitboxComponent)
+
 var player: Player
 const  FLASH_MATERIAL = preload("uid://x4frie3idpxo")
+const FLOATING_TEXT_SCENE = preload("uid://qkwyrg22h3gi")
 
 func get_chance_succes(chance: float) -> bool:
 	var random := randf_range(0.0, 1.0)
--- scenes/units/unit.gd
+++ scenes/units/unit.gd
@@ -22,11 +22,12 @@ func _on_hurtbox_component_on_damaged(hitbox: HitboxComponent) -> void:
 	
 	var blocked := Global.get_chance_succes(stats.block_chance / 100)
 	if blocked:
-		print('blocked')
+		Global.on_create_block_text.emit(self)
 		return
 	
 	set_flush_material()
 	health_component.take_damage(hitbox.damage)
+	Global.on_create_damage_text.emit(self, hitbox)
 
 
 func _on_flash_timer_timeout() -> void:
