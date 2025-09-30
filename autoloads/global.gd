a0376c65b430b807e8946923b594f305973f38f4
Tuskub <domenik.4@mail.ru>
2025-09-30 09:36:17 +0500

    Implement visual damage feedback (health bar + screen flash)

--- autoloads/global.gd
+++ autoloads/global.gd
@@ -1,3 +1,4 @@
 extends Node
 
 var player: Player
+const  FLASH_MATERIAL = preload("uid://x4frie3idpxo")
--- scenes/units/unit.gd
+++ scenes/units/unit.gd
@@ -7,13 +7,22 @@ class_name Unit
 @onready var sprite: Node2D = %Sprite
 @onready var animation: AnimationPlayer = $AnimationPlayer
 @onready var health_component: HealthComponent =$HealthComponent
+@onready var flash_timer: Timer = $FlashTimer
 
 func _ready() -> void:
 	health_component.setup(stats)
 
+func set_flush_material() -> void:
+	sprite.material = Global.FLASH_MATERIAL
+	flash_timer.start()
 
 func _on_hurtbox_component_on_damaged(hitbox: HitboxComponent) -> void:
 	if health_component.current_health <= 0.0:
 		return
 	
+	set_flush_material()
 	health_component.take_damage(hitbox.damage)
+
+
+func _on_flash_timer_timeout() -> void:
+	sprite.material = null
