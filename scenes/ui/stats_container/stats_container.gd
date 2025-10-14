aadb369d29ccdfadb37c6fb51bc23e98e11de065
Tuskub <domenik.4@mail.ru>
2025-10-14 13:06:19 +0500

    feat: create stats container

---
+++ scenes/ui/stats_container/stats_container.gd
@@ -0,0 +1,11 @@
+extends Panel
+class_name StatsContainer
+
+@onready var health_label: Label = %HealthLabel
+@onready var hp_regen_label: Label = %HPRegenLabel
+@onready var live_steal_label: Label = %LiveStealLabel
+@onready var damage_label: Label = %DamageLabel
+@onready var luck_label: Label = %LuckLabel
+@onready var speed_label: Label = %SpeedLabel
+@onready var block_label: Label = %BlockLabel
+@onready var harvesting_label: Label = %HarvestingLabel
