d0e0f0e8c5239886df5759b8c57ddce2c7ef3002
Tuskub <domenik.4@mail.ru>
2025-10-15 12:07:07 +0500

    feat: create update card and panel

--- scenes/ui/stats_container/stats_container.gd
+++ scenes/ui/stats_container/stats_container.gd
@@ -3,9 +3,25 @@ class_name StatsContainer
 
 @onready var health_label: Label = %HealthLabel
 @onready var hp_regen_label: Label = %HPRegenLabel
-@onready var live_steal_label: Label = %LiveStealLabel
+@onready var life_steal_label: Label = %LifeStealLabel
 @onready var damage_label: Label = %DamageLabel
 @onready var luck_label: Label = %LuckLabel
 @onready var speed_label: Label = %SpeedLabel
 @onready var block_label: Label = %BlockLabel
 @onready var harvesting_label: Label = %HarvestingLabel
+
+
+func	 _process(delta: float) -> void:
+	var player := Global.player
+	if not is_instance_valid(Global.player):
+		return
+	
+	var stast = player.stats
+	health_label.text = str(stast.health)
+	hp_regen_label.text = str(stast.hp_regen)
+	life_steal_label.text = str(stast.life_steal) + '%'
+	damage_label.text = str(stast.damage)
+	luck_label.text = str(stast.luck) + '%'
+	speed_label.text = str(stast.speed)
+	block_label.text = str(stast.block_chance) + '%'
+	harvesting_label.text = str(stast.harvesting)
