777d8e3e0dacd1f455d36eb6a31fbe5b7d0d72d8
Tuskub <domenik.4@mail.ru>
2025-10-22 00:40:19 +0500

    feat: implement shop panel

--- scenes/ui/shop_card/shop_card.gd
+++ scenes/ui/shop_card/shop_card.gd
@@ -1,2 +1,8 @@
 extends Panel
 class_name ShopCard
+
+@onready var item_icon: TextureRect = %ItemIcon
+@onready var item_name: Label = %ItemName
+@onready var item_type: Label = %ItemType
+@onready var item_description: RichTextLabel = %ItemDescription
+@onready var coins_label: Label = %CoinsLabel
